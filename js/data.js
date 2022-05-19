7b2089712d17cd09756d92227f0365ccd72258a2
Giovanni Ciotta <giociotta@gmail.com>
2022-05-19 22:43:43 +0200

    colori icone

--- js/data.js
+++ js/data.js
@@ -114,29 +114,72 @@ const icons = [
 ];
 
 const container = document.querySelector(".container");
- 
-
-
-
+const cardIcon = document.createElement ("div");
 // creo un ciclo per stampare le icone
 icons.forEach((myicone)=> {
-	let classePiuicons =  myicone.prefix + myicone.name ;
-	let classePiuFam =  myicone.family ; 
+	
+		let classePiuicons =  myicone.prefix + myicone.name ;
+	let classePiuFam =  myicone.family ;
+	let iconaNomeText = myicone.name;
+
 	console.log(classePiuicons);
 	console.log(classePiuFam);
 	
-	const cardIcon = document.createElement ("div");
+	
 	let iconaStamp = document.createElement ("i");
+	let iconaNome = document.createElement ("h3");
+	
+	cardIcon.append(iconaNome);
 	iconaStamp.classList.add ( classePiuFam );
 	iconaStamp.classList.add ( classePiuicons );
 	container.append(cardIcon);
 	cardIcon.append(iconaStamp);
+	
+	
+	
+	if (myicone.color === `blue`){
+		 iconaStamp.classList.add ("blu");
+		return true;
+	} 
+if (myicone.color === `orange`){
+			 iconaStamp.classList.add ( "arancio" );
+			return true;
+		} 
+if (myicone.color === `green`){
+				 iconaStamp.classList.add ( "verde" );
+				return true;
+			} 
 
 
+
+		
 	
 });
 console.log(icons);
 
+
+// aggiungere il colore alle icone 
+const coloreblu = icons.filter((mycolori) => {
+	
+	
+});
+	console.log("colore blu", coloreblu);
+	
+		
+	const colorearancio = icons.filter((mycolori) => {
+	
+		
+	});
+		console.log("colore arancio", colorearancio);
+		
+		const coloreverde = icons.filter((mycolori) => {
+	
+			
+		});
+			console.log("colore verde", coloreverde);
+ 
+
+			
 
 
 
